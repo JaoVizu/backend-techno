e5774fa0f635efe17b3846064bd942c5bb158f22
JaoVizu <joaovizu2015@gmail.com>
2020-10-30 14:18:35 -0300

    Buscando produto por uuid

--- src/routes.ts
+++ src/routes.ts
@@ -20,8 +20,21 @@ routes.get('/produtos', (req, res) => {
   
 })
 
-routes.get('/produtos/:id', (req, res) => {
-  
+routes.get('/produtos/:uuid', (req, res) => {
+  const id = req.param('uuid')
+  async function selectProduct(){
+    const client = await pool.connect()
+    try{
+      const result = await pool.query('SELECT * FROM produtos WHERE id = $1', [id])
+      return result.rows[0]
+    }catch(ex){
+      console.log(`Ocorreu um erro durante a consulta: ${ex}`)
+    }finally{
+      client.release()
+    }
+  }
+
+  selectProduct().then(data => res.json(data))
 })
 
 routes.post('/produtos', (req, res) => {
@@ -38,10 +51,10 @@ routes.post('/produtos', (req, res) => {
   async function insertProduct(){
     const client = await pool.connect()
     try{
-      const result = await pool.query('INSERT INTO produtos(id, nome, preco, descricao, estoque, imgpath) VALUES($1, $2, $3, $4, $5, $6) RETURNING *', [id,nome, preco, descricao, estoque, imgpath])
+      await pool.query('INSERT INTO produtos(id, nome, preco, descricao, estoque, imgpath) VALUES($1, $2, $3, $4, $5, $6) RETURNING *', [id,nome, preco, descricao, estoque, imgpath])
       res.json({message: "Produto inserido com sucesso!"})
     }catch(ex){
-      console.log(`Ocorreu um erro durante a consulta: ${ex}`)
+      console.log(`Ocorreu um erro durante o cadastro do produto: ${ex}`)
     }finally{
       client.release()
     }
